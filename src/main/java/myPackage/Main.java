9b1291b290149b92d1cf06ce34deae68fc276a3d
ManolisSof <manos.sofikitis@gmail.com>
2020-02-23 22:09:33 +0200

    Successfully created a KafkaFlink Producer and managed to pass data from a .csv file to Flink through a Kafka topic.

--- src/main/java/myPackage/Main.java
+++ src/main/java/myPackage/Main.java
@@ -2,13 +2,17 @@ package myPackage;
 
 
 import org.apache.commons.io.FileUtils;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple6;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 
 public class Main {
@@ -16,12 +20,22 @@ public class Main {
 
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // Set global parallelism
-        env.setParallelism(3);
+//        env.setParallelism(3);
+
+        // Create a Flink-Kafka Producer configurations to pass data from a topic to flink
+        String bootsrtapServers = "127.0.0.1:9092";
+        String topic = "flink_3";
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrtapServers);
+        // Read data from kafka topic
+        DataStream<String> kafkaData = env.addSource(new FlinkKafkaConsumer(topic, new SimpleStringSchema(), properties));
+
+
         // Read data from the file
-        DataStream<String> data = env.readTextFile("openaqFull.csv");
+//        DataStream<String> data = env.readTextFile("openaqFull.csv");
 
 //        DataStream<Tuple5<String, String, Double, Double, Integer>> mapped = data.map(new Splitter());
-        DataStream<Tuple6<String, String, Double, Double, Double, Integer>> avgVarData = data
+        DataStream<Tuple6<String, String, Double, Double, Double, Integer>> avgVarData = kafkaData
                 .map(new Splitter()) // Split data and keep only relevant attributes
                 .keyBy(new int[]{0, 1}) // groupBy country and parameter
                 .reduce(new Reduce1()) // Reduce. New tuples will be at the format of Country, Parameter, Measurement, Measurement, Measurement, Count
@@ -29,7 +43,7 @@ public class Main {
 
         // Now we have to try to add all gamma(i) and get the s(i) for each stratum.
 
-//        avgVarData.print();
+        avgVarData.print();
         // Delete output file if it already exists and create the output file.
         try {
             FileUtils.deleteDirectory(Paths.get("/home/manolis/Desktop/Special Topics on Databases/openaq data/GroupByCity_firstPass").toFile());
@@ -42,7 +56,7 @@ public class Main {
             System.out.println("Invalid permissions.");
         }
 
-        avgVarData.writeAsCsv("/home/manolis/Desktop/Special Topics on Databases/openaq data/GroupByCity_firstPass");
+//        avgVarData.writeAsCsv("/home/manolis/Desktop/Special Topics on Databases/openaq data/GroupByCity_firstPass");
 
         env.execute("Avg Job");
 
